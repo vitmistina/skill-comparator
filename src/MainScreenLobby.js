60210c865a4e8174697dbe80082e6421ef2f4971
Vit Mistina <vit.mistina@gmail.com>
2020-02-18 18:54:58 +0100

    moved content from live comparator to Lobby

---
+++ src/MainScreenLobby.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "./MainScreenLobby.css";
+import QRCodeImage from "./QRCodeImage";
+
+class MainScreenLobby extends Component {
+  render() {
+    return (
+      <div className="liveComparatorGrid">
+        <div className="comparatorGrid">
+          <div className="playersDiv">
+            <h2 className="display-4">Waiting for players</h2>
+            <h5>Players online: 9</h5>
+          </div>
+          <div className="qrCodeOutside">
+            <QRCodeImage />
+          </div>
+          <div className="area">
+            {/* <ProgressBar
+              now={70}
+              animated="true"
+              label="70%"
+              className="progressBar"
+            /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainScreenLobby;
